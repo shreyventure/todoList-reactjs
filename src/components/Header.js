8cbc7904d01b8b83eb0490c61384d103d33945fd
Shreyas Shrawage <55741087+shreyventure@users.noreply.github.com>
2020-12-04 19:52:27 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from "react-bootstrap";
 const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="/">TODO List</Navbar.Brand>
+      <Navbar.Brand href="/">TO-DO List</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
